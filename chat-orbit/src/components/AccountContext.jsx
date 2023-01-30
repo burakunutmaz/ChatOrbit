10d0490d57649bc4a76e0739302ca5f4b50e071f
Burak Unutmaz <burakunutmaz32@gmail.com>
2023-01-30 13:29:08 +0400

    Production

--- chat-orbit/src/components/AccountContext.jsx
+++ chat-orbit/src/components/AccountContext.jsx
@@ -8,7 +8,7 @@ const UserContext = ({children}) => {
     const navigate = useNavigate();
     const [user, setUser] = useState({loggedIn: null});
     useEffect(() => {
-        fetch("http://localhost:4000/auth/login", {
+        fetch(process.env.REACT_APP_SERVER_URL+"/auth/login", {
             credentials: "include"
         })
         .catch(err => {
